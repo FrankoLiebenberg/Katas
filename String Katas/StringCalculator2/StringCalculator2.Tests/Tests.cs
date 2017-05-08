8a30bdf48827fdd57ac6525846574128045bfd99
Franko Liebenberg <franko.liebenberg@kaleidocode.co.za>
2017-05-08 12:09:01 +0200

    Completed second Kata

---
+++ String Katas/StringCalculator2/StringCalculator2.Tests/Tests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using StringCalculator2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator2.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void GivenEmptyString_ShouldReturnZero()
+        {
+            Calculator calc = new Calculator();
+
+            var result = calc.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GivenOne_ShouldReturnOne()
+        {
+            Calculator calc = new Calculator();
+
+            var result = calc.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GivenOneAndTwo_ShouldReturnThree()
+        {
+            Calculator calc = new Calculator();
+
+            var result = calc.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void GivenRandomNumbers_ShouldReturnSum()
+        {
+            Calculator calc = new Calculator();
+
+            Random rand = new Random();
+            List<int> randomNumbers = new List<int>();
+            int upperBounds = rand.Next(1, 1001);
+            string numbers = "";
+
+            for (int i = 0; i < upperBounds; i++)
+            {
+                randomNumbers.Add(rand.Next(1, 1000));
+            }
+
+            foreach (int num in randomNumbers)
+            {
+                if (numbers == "")
+                    numbers = Convert.ToString(num);
+                else
+                    numbers += "," + num;
+            }
+
+            var result = calc.Add(numbers);
+
+            Assert.AreEqual(randomNumbers.Sum(), result);
+        }
+
+        [Test]
+        public void GivenOneTwoThreeAndNewLine_ShouldReturnSix()
+        {
+            Calculator calc = new Calculator();
+
+            var result = calc.Add("1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void GivenOneTwoThreeAndDifferentDelimiter_ShouldReturnSix()
+        {
+            Calculator calc = new Calculator();
+
+            var result = calc.Add("//;\n1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Add_GivenNegative_ShouldThrowException()
+        {
+            Calculator calc = new Calculator();
+
+            Assert.That(() => calc.Add("-100,-300"), Throws.Exception);
+
+
+        }
+    }
+}
---
+++ String Katas/StringCalculator2/StringCalculator2/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator2
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            numbers = checkIfEmptyString(numbers);
+            int[] splitNumbers = splitAndConvert(numbers);
+            checkIfNegative(splitNumbers);
+
+            return splitNumbers.Sum();
+        }
+
+        public string checkIfEmptyString(string numbers)
+        {
+            if (numbers == "")
+                numbers = "0";
+
+            return numbers;
+        }
+
+        public int[] splitAndConvert(string numbers)
+        {
+            char delimiter = getDelimiter(numbers);
+            string[] split = numbers.Split(new Char[] { ',', '\n', delimiter });
+            int[] convertedNumbers = new int[split.Count()];
+
+            for (int i = 0; i < split.Count(); i++)
+            {
+                if (split[i] != "//" && split[i] != "")
+                    convertedNumbers[i] = Convert.ToInt32(split[i]);
+            }
+
+            return convertedNumbers;
+        }
+
+        public char getDelimiter(string numbers)
+        {
+            char delimiter = ',';
+
+            if (numbers.Contains("//"))
+            {
+                string[] splitOnNewLine = numbers.Split('\n');
+                delimiter = Convert.ToChar(splitOnNewLine[0].Substring(2));
+            }
+
+            return delimiter;
+        }
+
+
+        public void checkIfNegative(int[] splitNumbers)
+        {
+            string message = "Negatives not allowed: ";
+
+            foreach (int num in splitNumbers)
+            {
+                if (num < 0)
+                {
+                    if (!message.Contains("-"))
+                        message += Convert.ToString(num);
+
+                    else
+                        message += "," + num;
+                }
+            }
+            if (message.Contains("-"))
+                throw new CustomException(message);
+        }
+
+    }
+}
